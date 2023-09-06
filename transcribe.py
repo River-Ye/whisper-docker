ef6bce40bfb2cced22e0ef8764ef331bc04b5ac5
River-Ye <river800111@gmail.com>
2023-09-06 15:11:28 +0800

    Modify print mode

--- transcribe.py
+++ transcribe.py
@@ -8,7 +8,6 @@ def get_args():
 
     parser.add_argument('--model', type=str, default='base')
     parser.add_argument('--input_file', type=str, default='input.mp3')
-    parser.add_argument('--output_file', type=str, default='output.txt')
     parser.add_argument('--language', type=str, default='ja')
 
     args = parser.parse_args()
@@ -21,15 +20,13 @@ def main():
 
     model_name = args.model
     input_file = args.input_file
-    output_file = args.output_file
     language = args.language
 
     whisper_model = whisper.load_model(model_name, download_root='./')
 
     result = whisper_model.transcribe(input_file, verbose=True, language=language)
 
-    with open(output_file, mode='w') as f:
-        f.write(result['text'])
+    print(result['text'])
 
 
 if __name__ == "__main__":
